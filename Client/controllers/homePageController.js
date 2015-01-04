00901191f53b2416746ae98191c709c9b7da91e8
Marianna Vladimirova <vvladimirova93@gmail.com>
2015-01-04 16:00:27 +0200

    Fixed problem with the folders

---
+++ Client/controllers/homePageController.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Marianna on 28.12.2014 г..
+ */
+var eventPlanerApp = angular.module("eventPlanerApp",[]);
+
+eventPlanerApp.controller("mainPage", function($scope){
+    $scope.title = "Chronos - Event Planer"
+});
