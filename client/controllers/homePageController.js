4e48bc9d2abd63d752ebf0e5787e3ea37053b167
Marianna Vladimirova <vvladimirova93@gmail.com>
2015-01-04 15:31:08 +0200

    Delete homePageController.js

--- client/controllers/homePageController.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Marianna on 28.12.2014 г..
- */
-var eventPlanerApp = angular.module("eventPlanerApp",[]);
-
-eventPlanerApp.controller("mainPage", function($scope){
-    $scope.title = "Chronos - Event Planer"
-});
